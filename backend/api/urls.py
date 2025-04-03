09c15cf3088a983dc6fca92268875cb7b4716e8b
aquarius1905 <reiko6122@gmail.com>
2025-04-03 14:58:29 +0900

    url.py修正

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import ClientUserViewSet, TransportScheduleViewSet
+from api.views.client_user_view import ClientUserViewSet
+from api.views.transport_schedule_view import TransportScheduleViewSet
 
 router = DefaultRouter()
 router.register(r'users', ClientUserViewSet)
