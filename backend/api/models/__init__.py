7661242ab95cb9a28671b949b8394143afa5f443
aquarius1905 <reiko6122@gmail.com>
2025-04-05 20:18:41 +0900

    migrationファイル作成

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -1,2 +1,5 @@
 from api.models.client_user import ClientUser # ご利用者様
 from api.models.transport_schedule import TransportSchedule # 送迎スケジュール
+from api.models.facility import Facility # デイサービス等の施設
+from api.models.guardian import Guardian # 利用者の家族
+from api.models.staff import Staff # デイサービス等の職員
