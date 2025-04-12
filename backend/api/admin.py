b9065e321c7a1b34c03be7ae07b0a4e5524236cc
aquarius1905 <reiko6122@gmail.com>
2025-04-12 09:42:36 +0900

    ユーザー登録・編集機能を管理画面に追加

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from api.models import ClientUser, TransportSchedule, Facility, Staff
+from api.models import BaseUser, ClientUser, TransportSchedule, Facility, Staff
+from django.contrib.auth.admin import UserAdmin
 
+
+@admin.register(BaseUser)
+class StaffAdmin(UserAdmin):
+    # 編集画面に表示するフィールドセット
+    fieldsets = UserAdmin.fieldsets + (
+        ("カスタム項目", {"fields": ("user_type",)}),
+    )
+
+    # 新規作成画面に表示するフィールドセット
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("カスタム項目", {"fields": ("user_type",)}),
+    )
+
+    # 管理画面の一覧に表示したい場合は以下も追加
+    list_display = UserAdmin.list_display + ('user_type',)
 
 @admin.register(Facility)
 class FacilityAdmin(admin.ModelAdmin):
